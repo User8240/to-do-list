7cac51c17cbecb31f9f092117e93183b25807bcc
Grace Kostanich <fl5812597@gmail.com>
2022-07-27 14:03:21 -0700

    Add functionality to assign catergories to items
    Co-authored-by: Sandra Tena <sandratena06@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -3,6 +3,7 @@ using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
@@ -17,18 +18,20 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList();
+      List<Item> model = _db.Items.Include(item => item.Category).ToList();
       return View(model);
     }
 
     public ActionResult Create()
     {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
     }
 
     public ActionResult Edit(int id)
     {
-        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+        Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+        ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
         return View(thisItem);
     }
 
